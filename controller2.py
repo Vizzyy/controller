fb9534756754f8a0a2e21701ed19793d4b24b4d7
Vizzyy <barnabas.vizy@gmail.com>
2022-10-29 22:25:58 -0400

    switch to prolonged token

--- controller2.py
+++ controller2.py
@@ -371,7 +371,7 @@ def handle_ptz_api_req(button_position, push_state):
             reo_api.api_ctrl(channel=api_channel, op='ToPos', speed=speed, preset_id=home_position_index)
 
         if button_position == camera_w_led:
-            camera_w_led_state = 1 if camera_w_led_state == 0 else camera_w_led_state = 0
+            camera_w_led_state = 1 if camera_w_led_state == 0 else 0
             reo_api.api_ctrl(w_led_state=camera_w_led_state, cmd='SetWhiteLed')
 
     else:
