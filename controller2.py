7880351d5aa1b51c8f53dc2c2132da72cf934525
Vizzyy <barnabas.vizy@gmail.com>
2022-10-31 00:17:11 -0400

    add ptz default reset location

--- controller2.py
+++ controller2.py
@@ -394,7 +394,8 @@ def handle_ptz_api_req(button_position, push_state):
             else:
                 set_led_red(camera_w_led)
         else:
-            reo_api.api_ctrl(channel=api_channel, op='Stop')
+            if button_position not in [camera_home, camera_zm_in, camera_zm_out, camera_w_led, camera_home_reset]:
+                reo_api.api_ctrl(channel=api_channel, op='Stop')
             set_led_green(button_position)
 
 
