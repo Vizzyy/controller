e223e5be453976fe2130096d968f1804aa67009a
Vizzyy <barnabas.vizy@gmail.com>
2023-06-10 17:07:09 -0400

    Puppy cam addition

--- controller2.py
+++ controller2.py
@@ -458,8 +458,13 @@ def handle_ptz_api_req(button_position, push_state):
             reo_api.api_ctrl(channel=api_channel, op='ToPos', speed=speed, preset_id=home_position_index)
 
         if button_position == camera_w_led:
-            camera_w_led_state = 1 if camera_w_led_state == 0 else 0
-            reo_api.api_ctrl(channel=api_channel, w_led_state=camera_w_led_state, cmd='SetWhiteLed')
+            if camera_selected != 7:
+                camera_w_led_state = 1 if camera_w_led_state == 0 else 0
+                reo_api.api_ctrl(channel=api_channel, w_led_state=camera_w_led_state, cmd='SetWhiteLed')
+            else:
+                # we want to reuse the LED button for a second default position for the puppy cam
+                # position 0 should be "Couch"
+                reo_api.api_ctrl(channel=api_channel, op='ToPos', speed=speed, preset_id=0)
 
         if button_position == camera_home_reset:
             reo_api.api_ctrl(channel=api_channel, cmd='SetPtzPreset')
